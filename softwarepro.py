6d563d1af22cc52f1c1e4a47a958959c9916ef32
Alankritha-24 <54307560+Alankritha-24@users.noreply.github.com>
2019-10-02 01:53:03 +0530

    Update softwarepro.py

--- softwarepro.py
+++ softwarepro.py
@@ -6,18 +6,18 @@ x = cp.Variable()
 y = cp.Variable()
 
 # Create two constraints.
-constraints = [(x)**2 + (y)**2 <= 16,
-                x>= 0,
-                x*np.sqrt(3) + y >=0]
+constraints = [(x1)**2 + (x2)**2 <= 16,
+                x1>= 0,
+                x1*np.sqrt(3) + x2 >=0]
 
 # Form objective.
-obj = cp.Minimize((x - 1)**2 + (y+3)**2)
+obj = cp.Minimize((x1 - 1)**2 + (x2 +3)**2)
 
 # Form and solve problem.
 prob = cp.Problem(obj, constraints)
 prob.solve()  # Returns the optimal value.
 print("status:", prob.status)
 print("optimal value", prob.value)
-print("optimal var", x.value, y.value)
+print("optimal var", x1.value, x2.value)
 p = prob.value
 print(np.sqrt(p))
